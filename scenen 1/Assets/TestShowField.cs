2c2112f3b25e70814a7517f1ad95299142801716
Mai Chi <nguyenmaichi0817@gmail.com>
2026-05-18 22:46:13 +0700

    first upload

---
+++ scenen 1/Assets/TestShowField.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class TestShowField : MonoBehaviour
+{
+    public Transform target;
+}
---
+++ scenen 1/Assets/enviroment/code/UpdateShaderProperties.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UpdateShaderProperties : MonoBehaviour
+{
+ 
+
+    void Update()
+    {
+        Shader.SetGlobalVector("_LightDir", transform.forward);
+    }
+}
